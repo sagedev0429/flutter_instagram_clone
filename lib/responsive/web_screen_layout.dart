2fb65857a354fed2ab114ca553fe06b811b6cf3a
binkyo1116 <topdevprince@gmail.com>
2022-12-29 23:45:33 -0800

    finish

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -1,12 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/global_variable.dart';
 
-class WebScreenLayout extends StatelessWidget {
+class WebScreenLayout extends StatefulWidget {
   const WebScreenLayout({super.key});
 
   @override
+  State<WebScreenLayout> createState() => _WebScreenLayoutState();
+}
+
+class _WebScreenLayoutState extends State<WebScreenLayout> {
+  final PageController pageController = PageController();
+
+  int _page = 0;
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  void jumpToPage(int page) {
+    pageController.jumpToPage(page);
+    setState(() {
+      _page = page;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('This is web')),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: SvgPicture.asset(
+          'assets/ic_instagram.svg',
+          color: primaryColor,
+          height: 32,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.home,
+              color: _page == 0 ? primaryColor : secondaryColor,
+            ),
+            onPressed: () => jumpToPage(0),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: _page == 1 ? primaryColor : secondaryColor,
+            ),
+            onPressed: () => jumpToPage(1),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.add_circle,
+              color: _page == 2 ? primaryColor : secondaryColor,
+            ),
+            onPressed: () => jumpToPage(2),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: _page == 3 ? primaryColor : secondaryColor,
+            ),
+            onPressed: () => jumpToPage(3),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.person,
+              color: _page == 4 ? primaryColor : secondaryColor,
+            ),
+            onPressed: () => jumpToPage(4),
+          ),
+        ],
+      ),
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        children: homeScreenItems,
+      ),
     );
   }
 }
--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/global_variable.dart';
+import 'package:instagram_clone/utils/utils.dart';
 import 'package:instagram_clone/widgets/post_card.dart';
 
 class FeedScreen extends StatefulWidget {
@@ -15,22 +17,24 @@ class _FeedScreenState extends State<FeedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        title: SvgPicture.asset(
-          'assets/ic_instagram.svg',
-          color: primaryColor,
-          height: 32,
-        ),
-        actions: [
-          IconButton(
-            onPressed: (() {}),
-            icon: const Icon(
-              Icons.messenger_outline,
+      appBar: webScreenSize < getScreenSize(context).width
+          ? null
+          : AppBar(
+              backgroundColor: mobileBackgroundColor,
+              title: SvgPicture.asset(
+                'assets/ic_instagram.svg',
+                color: primaryColor,
+                height: 32,
+              ),
+              actions: [
+                IconButton(
+                  onPressed: (() {}),
+                  icon: const Icon(
+                    Icons.messenger_outline,
+                  ),
+                )
+              ],
             ),
-          )
-        ],
-      ),
       body: StreamBuilder(
         stream: FirebaseFirestore.instance.collection('posts').snapshots(),
         builder: (context,
@@ -43,6 +47,12 @@ class _FeedScreenState extends State<FeedScreen> {
           return ListView.builder(
             itemCount: snapshot.data!.docs.length,
             itemBuilder: (context, index) => Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: getScreenSize(context).width > webScreenSize
+                    ? getScreenSize(context).width / 3
+                    : 0,
+                vertical: getScreenSize(context).width > webScreenSize ? 20 : 0,
+              ),
               child: PostCard(
                 snap: snapshot.data!.docs[index].data(),
               ),
